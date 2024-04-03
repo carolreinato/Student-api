e43b1d27cc83ed87de8b27c401b7b1841bf8881c
Caroline Reinato <cr.reinato@gmail.com>
2024-04-03 20:17:35 -0300

    Add project files.

---
+++ Student.Infra.Data/Context/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student.Infra.Data.Mapping;
+
+namespace Student.Infra.Data.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Entities.Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Domain.Entities.Student>(new StudentMap().Configure);
+        }
+    }
+}
---
+++ Student.Infra.Data/Mapping/StudentMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Infra.Data.Mapping
+{
+    public class StudentMap : IEntityTypeConfiguration<Domain.Entities.Student>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.Student> builder)
+        {
+            builder.ToTable("Students");
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Hash);
+        }
+    }
+}
